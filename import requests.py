66718f08bd733a01cdcff6266c38d63e6ee72f6b
MrmohamedMS <mohamedelsayed3876@gmail.com>
2023-04-08 15:00:13 +0200

    to send img

---
+++ import requests.py
@@ -0,0 +1,15 @@
+import requests
+
+# set the URL of the receiving website's endpoint that handles the image data
+url = 'https://example.com/receive-image'
+
+# set the path of the image you want to send
+image_path = '/path/to/your/image.jpg'
+
+# read the image data as binary and send a POST request to the receiving website
+with open(image_path, 'rb') as f:
+    image_data = f.read()
+    response = requests.post(url, data=image_data)
+
+# print the response from the receiving website
+print(response.text)
